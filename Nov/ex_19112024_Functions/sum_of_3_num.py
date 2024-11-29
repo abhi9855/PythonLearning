f4447a2dbd2cb7f689a68543eb9655a530be126e
Abhishek <abhisheksingh9855@gmail.com>
2024-11-29 19:08:31 +0530

    code correction

--- Nov/ex_19112024_Functions/sum_of_3_num.py
+++ Nov/ex_19112024_Functions/sum_of_3_num.py
@@ -1,9 +1,14 @@
+# Create a program to sum of three number from the user input,
+# if user doesn't enter any number', use default as 100, 200, 300
+
+# Logic Building
+
 try:
     num1 = int(input("Enter the num1: "))
     num2 = int(input("Enter the num2: "))
     num3 = int(input("Enter the num3: "))
 
-    def sumOfThreeNum(num1=100,num2=100,num3=100):
+    def sumOfThreeNum(num1=100,num2=200,num3=300):
         return num1+num2+num3
 
     result = sumOfThreeNum(num1, num2, num3)
@@ -16,7 +21,6 @@ except:
 else:
     print(f"The sum of {num1},{num2},{num3} : {result}")
 
-"""
-#result2 = sumOfThreeNum(10,20,30)
-print(result2)
-"""
+
+# result2 = sumOfThreeNum(10, num3=20)
+# print(result2)
